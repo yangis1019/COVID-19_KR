ad1e28adf938d4f24ee28fee06c1ae9d4f95f4a9
yangis1019 <50739218+yangis1019@users.noreply.github.com>
2020-02-25 18:24:42 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+site = urlopen("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=&brdGubun=&ncvContSeq=&contSeq=&board_id=&gubun=")
+resp = site.read()
+soup = BeautifulSoup(resp, "lxml")
+
+
+find = soup.find_all('ul', class_='s_listin_dot')
+find = [each_line.get_text().strip() for each_line in find[:20]]
+
+data = find[0].split("\n")
+
+def data_infected():
+    rawresult = ""
+    for i in range(7, len(data[0])-1):
+        rawresult += data[0][i]
+    result = rawresult.replace(",", "")
+    return int(result)
+
+def data_healed():
+    rawresult = ""
+    for i in range(12, len(data[1])-1):
+        rawresult += data[1][i]
+    result = rawresult.replace(",", "")
+    return int(result)
+
+def data_death():
+    rawresult = ""
+    for i in range(6, len(data[2])-1):
+        rawresult += data[2][i]
+    result = rawresult.replace(",", "")
+    return int(result)
+
+def data_checking():
+    rawresult = ""
+    for i in range(7, len(data[3])-1):
+        rawresult += data[3][i]
+    result = rawresult.replace(",", "")
+    return int(result)
